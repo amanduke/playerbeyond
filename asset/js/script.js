a91c4301238c185ceb768caf0f5693db241b5a94
amanduke <65379991+amanduke@users.noreply.github.com>
2020-09-17 20:18:18 -0400

    update

---
+++ asset/js/script.js
@@ -0,0 +1,17 @@
+fetch('https://api-v3.igdb.com/games?q=&api_key=21bc044a492256eb1717ed91dd67cbc1')
+
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+})
+
+// fetch('https://api-v3.igdb.com/games?q=&api_key=21bc044a492256eb1717ed91dd67cbc1')
+
+// .then(function(response) {
+//     return response.json();
+// })
+// .then(function(data) {
+//     console.log(data);
+// })
