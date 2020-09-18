7d99cc4c9c97fe1fa78eb1cc3dd931682330c172
AKen00 <ajken04@gmail.com>
2020-09-17 20:24:38 -0400

    script update

--- asset/script.js
+++ asset/script.js
@@ -1,5 +1,5 @@
 
-fetch('https://api-v3.igdb.com/games?q=&api_key=21bc044a492256eb1717ed91dd67cbc1')
+fetch('https://api-v3.igdb.com/user-key?q=21bc044a492256eb1717ed91dd67cbc1')
 
 .then(function(response) {
     return response.json();
