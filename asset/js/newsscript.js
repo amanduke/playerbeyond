433f128329aed2409cff3141a62347c12216ed86
AKen00 <ajken04@gmail.com>
2020-09-27 13:30:43 -0400

    Latest News Function

--- asset/js/newsscript.js
+++ asset/js/newsscript.js
@@ -9,80 +9,34 @@
 //     onclick="location.href='news.html';";
 // }
 
+document.getElementById("latestNews").onclick = function() {newsFunction()};
 
 function newsFunction(){
-    fetch('https://cors-anywhere.herokuapp.com/http://www.gamespot.com/api/articles/?api_key=d5f9d95899dd3f623ef0db6a138808c83f7967cd&format=json&filter=title:')
+    fetch('https://cors-anywhere.herokuapp.com/http://www.gamespot.com/api/articles/?api_key=d5f9d95899dd3f623ef0db6a138808c83f7967cd&format=json&sort=publish_date:desc')
 
     .then(function(newsResponse) {
         return newsResponse.json();
     })
 
     .then(function(newsResponse){
-        console.log(newsResponse.data[0]);
-        var newsHeader = document.querySelector("#news-header");
-        newsHeader.innerHTML = '';
+        console.log(newsResponse)
+        console.log(newsResponse.results[0]);
+
+
+        var resultsNewsTitle;
+        var resultsNewsDate;
+        var resultsNewsImage;
+        var resultsNewsBody;
+
+        for (i = 0; i <= 5; i++) {
+            resultsNewsTitle = newsResponse.results[i].title;
+            document.getElementById("news-title"+i).innerHTML = resultsNewsTitle;
+            resultsNewsDate = newsResponse.results[i].publish_date;
+            document.getElementById("news-date"+i).innerHTML = resultsNewsDate;
+            resultsNewsImage = newsResponse.results[i].image.original;
+            document.getElementById("news-image"+i).innerHTML = `<img src = ` + resultsNewsImage + `>`;
+            resultsNewsBody = newsResponse.results[i].body;
+            document.getElementById("news-body"+i).innerHTML = resultsNewsBody;
+        }
     })
 }
-
-
-window.addEventListener('load', function() {
-    var newsBar = document.querySelector("#news");
-
-    newsBar.addEventListener('keyup', function(event) {
-        if(event.key == 'Enter') {
-            
-            fetch('https://cors-anywhere.herokuapp.com/http://www.gamespot.com/api/games/?api_key=d5f9d95899dd3f623ef0db6a138808c83f7967cd&format=json&filter=title:'
-            + encodeURIComponent(newsBar.value)
-            )
-            
-            
-            .then(function(newsBar) {
-                return newsBar.json();
- 
-            })
-            .then(function(data) {
-                console.log(data);
-
-                var resultsName;
-                var resultsRel;
-                var resultsImage;
-                var resultsDeck;
-
-                for (i = 0; i <= 5; i++) {
-                    resultsName = data.results[i].name;
-                    document.getElementById("name"+i).innerHTML = resultsName;
-                    resultsRel = data.results[i].release_date;
-                    document.getElementById("release"+i).innerHTML = resultsRel;
-                    resultsImage = data.results[i].image.original;
-                    document.getElementById("image"+i).innerHTML = `<img src = ` + resultsImage + `>`;
-                    resultsDeck = data.results[i].deck;
-                    document.getElementById("deck"+i).innerHTML = resultsDeck;
-                }
-
-        
-                fetch('https://cors-anywhere.herokuapp.com/http://www.gamespot.com/api/reviews/?api_key=d5f9d95899dd3f623ef0db6a138808c83f7967cd&format=json&filter=title:'
-                + encodeURIComponent (newsBar.value))
-                
-                .then(function(results){
-                    return results.json();
-                    
-                })
-
-                .then(function(res) {
-                    console.log(res);
-                    console.log('test' + newsBar.value)
-
-                    var resultsDes;
-
-                    for (i = 0; i <= 5; i++){
-                    resultsDes = res.results[i].body;
-                    document.getElementById("description"+i).innerHTML = resultsDes;
-                    }
-
-                    
-                })
-            })
-            // searchBar.value = "" 
-        }
-    });
-});
